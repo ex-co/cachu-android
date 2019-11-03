d87c3a01c85616266a034638f0bee74a85e8349e
yegeeee <kangyeaji@naver.com>
2019-11-03 22:38:18 +0900

    add Map View (#1)

--- app/src/main/java/com/ex/cachu/MainActivity.kt
+++ app/src/main/java/com/ex/cachu/MainActivity.kt
@@ -2,11 +2,19 @@ package com.ex.cachu
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import net.daum.mf.map.api.MapView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val mapView = MapView(this)
+
+        val mapViewContainer = findViewById(R.id.map_view) as ViewGroup
+        mapViewContainer.addView(mapView)
     }
 }
