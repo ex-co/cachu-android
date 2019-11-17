9739618657d91b8ba24e35fbab9a4fe2020bc2e5
Jaewon Choi <qwebnm7788@naver.com>
2019-11-17 13:27:37 +0900

    [Feature-Login] Implement Kakao Login (#1)
    * [Feature-Login] Implement Kakao Login
    
    - Add Kakao Login feature
    - Add Codestyle for .xml, .kt files
    
    * [Feature-login] Add get userInfo logic
    
    - get user nickname and profile image

--- app/src/main/java/com/ex/cachu/ui/SplashActivity.kt
+++ app/src/main/java/com/ex/cachu/ui/SplashActivity.kt
@@ -3,19 +3,55 @@ package com.ex.cachu.ui
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.ex.cachu.R
+import com.ex.cachu.login.LoginActivity
+import com.kakao.auth.ISessionCallback
+import com.kakao.auth.Session
+import com.kakao.util.exception.KakaoException
 
 class SplashActivity : AppCompatActivity() {
-    private val SPLASH_TIME_OUT:Long=3000 // 3 sec
+
+    private val TAG: String = "SplashActivity"
+    private val SPLASH_TIME_OUT: Long = 3000 // 3 sec
+
+    private var callback: ISessionCallback? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        callback = object: ISessionCallback {
+            override fun onSessionOpened() {
+                Log.d(TAG, "onSessionOpened")
+                goToHomeActivity()
+            }
+
+            override fun onSessionOpenFailed(exception: KakaoException?) {
+                Log.d(TAG, "onSessionOpenFailed")
+                goToLoginActivity()
+            }
+        }
 
         Handler().postDelayed({
-            startActivity(Intent(this, HomeActivity::class.java))
+            if (!Session.getCurrentSession().checkAndImplicitOpen()) {
+                goToLoginActivity()
+            }
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, SPLASH_TIME_OUT)
+    }
+
+    private fun goToHomeActivity() {
+        val intent = Intent(this, HomeActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
     }
 }
